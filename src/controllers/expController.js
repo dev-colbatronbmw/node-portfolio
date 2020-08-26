cda2368285709c3b4d36424a0ee6277a76f02bcb
Colby Holmstead <colbatronbmw@gmail.com>
2020-08-26 14:53:27 -0700

    adding files for downloads

--- src/controllers/expController.js
+++ src/controllers/expController.js
@@ -8,9 +8,32 @@ function expController() {
 
     res.render("exp", {});
   }
-
+  function getMovieTicket(req, res) {
+    debug("Get Movie ticket: ", "Working");
+    const file = "./projects/cSharp/MovieObj.zip";
+    res.download(file);
+  }
+  function getStudent(req, res) {
+    debug("Get Student: ", "Working");
+    const file = "./projects/cSharp/Student-2.zip";
+    res.download(file);
+  }
+  function getUnityGameMac(req, res) {
+    debug("Get Student: ", "Working");
+    const file = "./projects/cSharp/transport1.zip";
+    res.download(file);
+  }
+  function getUnityGameWindows(req, res) {
+    debug("Get Student: ", "Working");
+    const file = "./projects/cSharp/TransportGame.zip";
+    res.download(file);
+  }
   return {
     getExp,
+    getMovieTicket,
+    getStudent,
+    getUnityGameWindows,
+    getUnityGameMac,
   };
 }
 module.exports = expController;
--- src/routes/expRoutes.js
+++ src/routes/expRoutes.js
@@ -6,10 +6,19 @@ const expController = require("../controllers/expController");
 const expRouter = express.Router();
 
 function router() {
-  const { getExp } = expController();
+  const {
+    getExp,
+    getMovieTicket,
+    getStudent,
+    getUnityGameWindows,
+    getUnityGameMac,
+  } = expController();
 
   expRouter.route("/").get(getExp);
-
+  expRouter.route("/MovieTicket").get(getMovieTicket);
+  expRouter.route("/StudentList").get(getStudent);
+  expRouter.route("/UnityGameWindows").get(getUnityGameWindows);
+  expRouter.route("/UnityGameMac").get(getUnityGameMac);
   return expRouter;
 }
 module.exports = router;
