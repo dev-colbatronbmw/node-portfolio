17c319d58a1fd224a1a6439fb11127002eca3e9a
Colby Holmstead <colbatronbmw@gmail.com>
2020-09-16 07:40:02 -0700

    taking back photos from imgur, looking at uploading my own photos.

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -15,7 +15,12 @@ const pool = mysql.createPool({
 function getConnection() {
   return pool;
 }
+function getimage(url) {
+  // image = `<blockquote class="imgur-embed-pub" lang="en" data-id="a/R0vMibX" data-context="false" ><a href="//imgur.com/a/R0vMibX"></a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>`;
 
+  // debug("regex output", link);
+  return url.match(/(a\/.*?)\"/i)[1];
+}
 function blogController() {
   debug("blog controller: ", "working");
 
@@ -34,11 +39,14 @@ function blogController() {
 
       //code modification
       blog = rows.map(row => {
+        var image1 = getimage(row.image1);
+        debug("image 1 ", row.image1);
+        debug("image 1 after regex", image1);
         return {
           postId: row.postId,
           title: row.title,
           content: row.content,
-          image1: row.image1,
+          image1: image1,
           image2: row.image2,
           image3: row.image3,
           published: row.published,
@@ -46,7 +54,7 @@ function blogController() {
         };
       });
 
-      //   debug("posts in connection", blog);
+      // debug("posts in connection", blog);
 
       getConnection().query("SELECT * FROM blogComments", [], function (
         err,
@@ -85,9 +93,9 @@ function blogController() {
             };
           });
 
-          debug("posts", blog);
-          debug("comments", comments);
-          debug("likes", likes);
+          // debug("posts", blog);
+          // debug("comments", comments);
+          // debug("likes", likes);
 
           if (typeof req.session.passport !== "undefined") {
             res.render("./fun/blog-index", {
@@ -115,16 +123,105 @@ function blogController() {
 
     const postId = req.params.postId;
 
-    if (typeof req.session.passport !== "undefined") {
-      res.render("./fun/blog-show", {
-        csrfToken: req.csrfToken(),
-        user: req.session.passport.user
-      });
-    } else {
-      res.render("./fun/blog-show", {
-        csrfToken: req.csrfToken()
-      });
-    }
+    getConnection().query(
+      "SELECT * FROM blog WHERE postId = ?",
+      [postId],
+      function (err, rows) {
+        if (err) {
+          res.redirect("back");
+          return;
+        }
+
+        //code modification
+        blog = rows.map(row => {
+          return {
+            postId: row.postId,
+            title: row.title,
+            content: row.content,
+            image1: row.image1,
+            image2: row.image2,
+            image3: row.image3,
+            published: row.published,
+            userId: row.userId
+          };
+        });
+
+        // debug("posts in connection", blog);
+
+        getConnection().query(
+          "SELECT * FROM blogComments WHERE postId = ?",
+          [postId],
+          function (err, rows) {
+            if (err) {
+              res.redirect("back");
+              return;
+            }
+
+            //code modification
+            comments = rows.map(row => {
+              return {
+                commentId: row.commentId,
+                postId: row.postId,
+                content: row.content,
+                userId: row.userId
+              };
+            });
+
+            getConnection().query(
+              "SELECT * FROM blogLikes WHERE postId = ?",
+              [postId],
+              function (err, rows) {
+                if (err) {
+                  res.redirect("back");
+                  return;
+                }
+
+                //code modification
+                likes = rows.map(row => {
+                  return {
+                    likeId: row.likeId,
+                    postId: row.postId,
+                    userId: row.userId
+                  };
+                });
+
+                // debug("posts", blog);
+                // debug("comments", comments);
+                // debug("likes", likes);
+
+                if (typeof req.session.passport !== "undefined") {
+                  res.render("./fun/blog-show", {
+                    csrfToken: req.csrfToken(),
+                    user: req.session.passport.user,
+                    blog: blog,
+                    comments: comments,
+                    likes: likes
+                  });
+                } else {
+                  res.render("./fun/blog-show", {
+                    csrfToken: req.csrfToken(),
+                    blog: blog,
+                    comments: comments,
+                    likes: likes
+                  });
+                }
+              }
+            );
+          }
+        );
+      }
+    );
+
+    // if (typeof req.session.passport !== "undefined") {
+    //   res.render("./fun/blog-show", {
+    //     csrfToken: req.csrfToken(),
+    //     user: req.session.passport.user
+    //   });
+    // } else {
+    //   res.render("./fun/blog-show", {
+    //     csrfToken: req.csrfToken()
+    //   });
+    // }
   }
 
   return {
@@ -132,4 +229,5 @@ function blogController() {
     getBlogShow
   };
 }
+
 module.exports = blogController;
