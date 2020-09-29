b7346ba2d0339706add0e02c7131fd357d53ca05
Colby Holmstead <colbatronbmw@gmail.com>
2020-09-28 22:20:43 -0700

    change Password is now complete

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -307,124 +307,125 @@ function userController() {
     debug("Get postForgotPassword: ", "Working");
 
     const email = req.body.email;
-    // debug("email for reset", email);
+    debug("email for reset", email);
     getConnection().query(
       "SELECT * FROM users WHERE UserEmail = ?",
       [email],
-      function (err, rows) {
+      (err, rows) => {
         if (err) {
           req.flash("errorMessage", "Email not found");
           res.redirect("back");
-        }
-        users = rows.map(row => {
-          return {
-            userId: row.Id,
-            userName: row.UserName,
-            userEmail: row.UserEmail
-          };
-        });
-        const user = users[0];
-
-        if (typeof users[0] === "undefined") {
-          req.flash("errorMessage", "That user does not exist");
-          res.redirect("/User/Forgot/Password");
         } else {
-          // debug("users[0] from database", users[0]);
-          // debug("email from database", users[0].userEmail);
-          if (users[0].userEmail === email) {
-            // do the thing
-            var myRegex = /\/|\.|\?/g;
-            var regex = new RegExp(myRegex);
-            var token = bcrypt.hashSync(email, 10, null);
+          users = rows.map(row => {
+            return {
+              userId: row.Id,
+              userName: row.UserName,
+              userEmail: row.UserEmail
+            };
+          });
+          const user = users[0];
 
-            var match = regex.test(token);
-            // debug("match: ", match);
-            // debug("token before loop: ", token);
-            while (match) {
+          if (typeof users[0] === "undefined") {
+            req.flash("errorMessage", "That user does not exist");
+            res.redirect("/User/Forgot/Password");
+          } else {
+            // debug("users[0] from database", users[0]);
+            // debug("email from database", users[0].userEmail);
+            if (users[0].userEmail === email) {
+              // do the thing
+              var myRegex = /\/|\.|\?/g;
+              var regex = new RegExp(myRegex);
+              var token = bcrypt.hashSync(email, 10, null);
+
+              var match = regex.test(token);
+              // debug("match: ", match);
+              // debug("token before loop: ", token);
               while (match) {
                 while (match) {
-                  token = bcrypt.hashSync(email, 10, null);
+                  while (match) {
+                    token = bcrypt.hashSync(email, 10, null);
+                    match = regex.test(token);
+                    // debug("match center: ", match);
+                    // debug("token reset center: ", token);
+                  }
                   match = regex.test(token);
-                  // debug("match center: ", match);
-                  // debug("token reset center: ", token);
+                  // debug("match after center: ", match);
+                  // debug("token reset after center: ", token);
                 }
                 match = regex.test(token);
-                // debug("match after center: ", match);
-                // debug("token reset after center: ", token);
+                // debug("match last: ", match);
+                // debug("token reset last: ", token);
               }
-              match = regex.test(token);
-              // debug("match last: ", match);
-              // debug("token reset last: ", token);
-            }
-            debug("match selected: ", match);
-            debug("token selected: ", token);
+              debug("match selected: ", match);
+              debug("token selected: ", token);
 
-            // check token for bad chars if it has any reset
+              // check token for bad chars if it has any reset
 
-            req.flash(
-              "loginMessage",
-              "Email Sent! It may take a few minutes to arrive."
-            );
-            // add token and email to database
+              req.flash(
+                "loginMessage",
+                "Email Sent! It may take a few minutes to arrive."
+              );
+              // add token and email to database
 
-            getConnection().query(
-              " INSERT INTO resetTokens (resetEmail, resetToken) VALUES (?, ?)",
-              [email, token],
-              (err, results, fields) => {
-                if (err) {
-                  req.flash(
-                    "errorMessage",
-                    "Error getting reset please contact Colby"
-                  );
-                  res.redirect("back");
-                } else {
-                  //nodemailer build stringified token
+              getConnection().query(
+                " INSERT INTO resetTokens (resetEmail, resetToken) VALUES (?, ?)",
+                [email, token],
+                (err, results, fields) => {
+                  if (err) {
+                    req.flash(
+                      "errorMessage",
+                      "Error getting reset please contact Colby"
+                    );
+                    res.redirect("back");
+                  } else {
+                    //nodemailer build stringified token
 
-                  // ----------------------email registrant -------------------
-                  async function email() {
-                    // Generate test SMTP service account from ethereal.email
-                    // Only needed if you don't have a real mail account for testing
-                    let testAccount = await nodemailer.createTestAccount();
+                    // ----------------------email registrant -------------------
+                    async function email() {
+                      // Generate test SMTP service account from ethereal.email
+                      // Only needed if you don't have a real mail account for testing
+                      let testAccount = await nodemailer.createTestAccount();
 
-                    // create reusable transporter object using the default SMTP transport
-                    let transporter = nodemailer.createTransport({
-                      host: "smtp.dreamhost.com",
-                      port: 465,
-                      secure: true, // true for 465, false for other ports
-                      auth: {
-                        user: process.env.EMAIL_USER, // generated ethereal user
-                        pass: process.env.EMAIL_PASSWORD // generated ethereal password
-                      }
-                    });
-                    // debug("user being emailed: ", user);
-                    // send mail with defined transport object
-                    let info = await transporter.sendMail({
-                      from: '"Colby Holmstead" <dev@colbyholmstead.com>', // sender address
-                      to: `${user.userEmail}`, // list of receivers
-                      subject: "Password Change Link", // Subject line
-                      text: `
+                      // create reusable transporter object using the default SMTP transport
+                      let transporter = nodemailer.createTransport({
+                        host: "smtp.dreamhost.com",
+                        port: 465,
+                        secure: true, // true for 465, false for other ports
+                        auth: {
+                          user: process.env.EMAIL_USER, // generated ethereal user
+                          pass: process.env.EMAIL_PASSWORD // generated ethereal password
+                        }
+                      });
+                      // debug("user being emailed: ", user);
+                      // send mail with defined transport object
+                      let info = await transporter.sendMail({
+                        from: '"Colby Holmstead" <dev@colbyholmstead.com>', // sender address
+                        to: `${user.userEmail}`, // list of receivers
+                        subject: "Password Change Link", // Subject line
+                        text: `
               ${user.userName}, To change your password plaese paste this URL into your browser: http://localhost:4000/User/Forgot/Password/${token} \n
               If you were not the one who changed your password plase contact Colby at dev@colbyholmstead.com \n
               I will get back to you as soon as possible.
               `, // plain text body
-                      html: `<p>
+                        html: `<p>
 
               ${user.userName},</p>
                <p>To change your password plaese paste this URL into your browser: http://localhost:4000/User/Forgot/Password/${token}</p>
                <p> If you were not the one who changed your password plase contact Colby at dev@colbyholmstead.com</p>
               <p> I will get back to you as soon as possible. </p>` // html body
-                    });
+                      });
+                    }
+
+                    email().catch(console.error);
+
+                    res.redirect("/User/LogIn");
                   }
-
-                  email().catch(console.error);
-
-                  res.redirect("/User/LogIn");
                 }
-              }
-            );
-          } else {
-            req.flash("errorMessage", "Email not found");
-            res.redirect("back");
+              );
+            } else {
+              req.flash("errorMessage", "Email not found");
+              res.redirect("back");
+            }
           }
         }
       }
@@ -484,10 +485,10 @@ function userController() {
                 return {
                   userId: row.Id,
                   userName: row.UserName,
-                  userEmail: row.UserEmail,
-                  userPassword: row.UserPassword
+                  userEmail: row.UserEmail
                 };
               });
+
               // delete the token from the database
               debug("user token to delete: ", users[0]);
               getConnection().query(
@@ -502,7 +503,8 @@ function userController() {
 
                     // debug("user: ", users[0]);
                     res.render("user/forgot-password-reset", {
-                      csrfToken: req.csrfToken()
+                      csrfToken: req.csrfToken(),
+                      user: users[0]
                       // user: req.session.passport.user,
                       // message: req.flash("errorMessage")
                     });
@@ -520,13 +522,13 @@ function userController() {
     // debug("user: ", user);
   }
   function postForgotPasswordReset(req, res) {
-    const user = req.session.passport.user;
-    const oldPassword = req.body.oldPassword;
-    const newPassword = req.body.newPassword;
-    debug("user for password update", user);
+    const email = req.body.email;
+
+    const newPassword = req.body.password;
+    // debug("user for password update", user);
     getConnection().query(
       "SELECT * FROM users WHERE UserEmail = ?",
-      [user.UserEmail],
+      [email],
       function (err, rows) {
         // debug("user: ", rows[0]);
         if (err) {
@@ -534,74 +536,79 @@ function userController() {
           res.redirect("back");
         }
 
-        // if the user is found but the password is wrong
-        if (!bcrypt.compareSync(oldPassword, rows[0].UserPassword)) {
-          req.flash("errorMessage", "Oops! Wrong original password");
-          res.redirect("back");
-        } else {
-          // create the loginMessage and save it to session as flashdata
-
-          // all is well, return successful user
-          // debug("user about to log in: ", rows[0]);
-
-          var newHashedPassword = {
-            password: bcrypt.hashSync(newPassword, 10, null) // use the generateHash function in our user model
+        users = rows.map(row => {
+          return {
+            userId: row.Id,
+            userName: row.UserName,
+            userEmail: row.UserEmail
           };
+        });
+        const user = users[0];
 
-          getConnection().query(
-            "UPDATE users SET UserPassword = ? WHERE UserEmail = ?",
-            [newHashedPassword.password, user.UserEmail],
-            function (err, rows) {
-              // debug("user: ", rows[0]);
-              if (err) {
-                debug("error", err);
-                req.flash("errorMessage", err);
-                res.redirect("back");
-              } else {
-                // ----------------------email registrant -------------------
-                async function secondary() {
-                  // Generate test SMTP service account from ethereal.email
-                  // Only needed if you don't have a real mail account for testing
-                  let testAccount = await nodemailer.createTestAccount();
+        // if the user is found but the password is wrong
 
-                  // create reusable transporter object using the default SMTP transport
-                  let transporter = nodemailer.createTransport({
-                    host: "smtp.dreamhost.com",
-                    port: 465,
-                    secure: true, // true for 465, false for other ports
-                    auth: {
-                      user: process.env.EMAIL_USER, // generated ethereal user
-                      pass: process.env.EMAIL_PASSWORD // generated ethereal password
-                    }
-                  });
+        // create the loginMessage and save it to session as flashdata
 
-                  // send mail with defined transport object
-                  let info = await transporter.sendMail({
-                    from: '"Colby Holmstead" <dev@colbyholmstead.com>', // sender address
-                    to: `${user.UserEmail}`, // list of receivers
-                    subject: "Password Has Changed", // Subject line
-                    text: `
+        // all is well, return successful user
+        // debug("user about to log in: ", rows[0]);
+
+        var newHashedPassword = {
+          password: bcrypt.hashSync(newPassword, 10, null) // use the generateHash function in our user model
+        };
+
+        getConnection().query(
+          "UPDATE users SET UserPassword = ? WHERE UserEmail = ?",
+          [newHashedPassword.password, email],
+          function (err, rows) {
+            // debug("user: ", rows[0]);
+            if (err) {
+              debug("error", err);
+              req.flash("errorMessage", err);
+              res.redirect("back");
+            } else {
+              // ----------------------email registrant -------------------
+              async function email() {
+                // Generate test SMTP service account from ethereal.email
+                // Only needed if you don't have a real mail account for testing
+                let testAccount = await nodemailer.createTestAccount();
+
+                // create reusable transporter object using the default SMTP transport
+                let transporter = nodemailer.createTransport({
+                  host: "smtp.dreamhost.com",
+                  port: 465,
+                  secure: true, // true for 465, false for other ports
+                  auth: {
+                    user: process.env.EMAIL_USER, // generated ethereal user
+                    pass: process.env.EMAIL_PASSWORD // generated ethereal password
+                  }
+                });
+
+                // send mail with defined transport object
+                let info = await transporter.sendMail({
+                  from: '"Colby Holmstead" <dev@colbyholmstead.com>', // sender address
+                  to: `${user.UserEmail}`, // list of receivers
+                  subject: "Password Has Changed", // Subject line
+                  text: `
                     ${user.UserName}, Your password has been changed.\n
-                     If you were not the one who changed your password plase contact admin at dev@colbyholmstead.com \n
+                     If you were not the one who changed your password plase contact Colby at dev@colbyholmstead.com \n
                     I will get back to you as soon as possible.
                     `, // plain text body
-                    html: `<p> 
+                  html: `<p> 
     
                     ${user.UserName},</p>
                      <p>Your password has been changed.</p> 
-                     <p> If you were not the one who changed your password plase contact admin at dev@colbyholmstead.com</p>
+                     <p> If you were not the one who changed your password plase contact Colby at dev@colbyholmstead.com</p>
                     <p> I will get back to you as soon as possible. </p>` // html body
-                  });
-                }
-
-                secondary().catch(console.error);
-
-                req.flash("loginMessage", "Password Updated");
-                res.redirect("/User/Profile");
+                });
               }
+
+              email().catch(console.error);
+
+              req.flash("loginMessage", "Password Updated");
+              res.redirect("/User/Profile");
             }
-          );
-        }
+          }
+        );
       }
     );
   }
